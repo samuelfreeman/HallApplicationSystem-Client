aecffd25369c727d367c8fc547c110143ab14861
samuelfreeman <samuelmanueljnr@gmail.com>
2024-12-22 18:59:32 +0000

    hot fix

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,19 +1,8 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'
+
 const HomePage = () => {
 
 
-  const navigate = useNavigate();
-  useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-    console.log(storedToken)
 
-
-    if (storedToken === undefined || storedToken === null) {
-      navigate("/login");
-
-    }
-  }, [navigate])
 
   return (
     <div className="w-full h-screen bg-black flex justify-center items-center">
