f9fbdf16b33bcc924e401c10e71d84e416050734
samuelfreeman <samuelmanueljnr@gmail.com>
2025-01-03 21:15:54 +0000

    added the url

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://hallapplicationsystem.onrender.com/",
   timeout: 100000,
   headers: {
     "Content-Type": "application/json",
